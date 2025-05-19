2dacb1d1d095b8b5a5da14cda9849229d6f3baa8
FrJustAChillGuy <tsrahma216@pccsk12.com>
2025-05-19 09:11:47 -0400

    added box2D library

---
+++ core/src/main/java/com/puzzle_game/Main.java
@@ -0,0 +1,91 @@
+package com.puzzle_game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+
+    private Texture backgroundTexture;
+    Texture characterTexture; // TODO: fix the background of the character
+
+    Sprite playerSprite;
+
+    FitViewport viewport;
+
+    @Override
+    public void create() {
+
+        batch = new SpriteBatch();
+        backgroundTexture = new Texture("background.png");
+        characterTexture = new Texture("Whiteboxguy.png");
+
+        playerSprite = new Sprite(characterTexture);
+        playerSprite.setSize(1, 1);
+
+        viewport = new FitViewport(16, 10);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() { // get keyboard inputs
+
+        float speed = 2.0f;
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            playerSprite.translateX(speed * delta);
+        } else if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            playerSprite.translateX(-speed * delta);
+        }
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+
+        ScreenUtils.clear(Color.BLACK);
+        
+        viewport.apply();
+
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        batch.begin();
+        
+        batch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        playerSprite.draw(batch);
+
+        batch.end();
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        backgroundTexture.dispose();
+    }
+}
